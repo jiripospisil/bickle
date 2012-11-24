ac2ea16b3fe0b05a7ed1a2f3e1f990a93b4281d7
Jiri Pospisil <mekishizufu@gmail.com>
2012-11-24 20:46:59 +0100

    Require bickle in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,26 @@
+require "bundler"
+Bundler.setup
+
+require "bickle"
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
   config.order = "random"
+
+  # https://github.com/wycats/thor/blob/master/spec/spec_helper.rb#L41
+  def capture(stream)
+    begin
+      stream = stream.to_s
+      eval "$#{stream} = StringIO.new"
+      yield
+      result = eval("$#{stream}").string
+    ensure
+      eval("$#{stream} = #{stream.upcase}")
+    end
+
+    result
+  end
 end
